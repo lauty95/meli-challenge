5e99731575266b9ba70591f5effcd8ac034ac92c
Lauty <38353324+lauty95@users.noreply.github.com>
2024-03-10 22:57:19 -0300

    Control de existencia de producto

--- client/src/views/ItemDetails.jsx
+++ client/src/views/ItemDetails.jsx
@@ -16,7 +16,7 @@ function ItemDetails () {
         })
         .catch((err) => {
           console.error(err)
-          setItem(null)
+          setItem({ error: true })
         })
     }
   }, [id])
@@ -26,30 +26,32 @@ function ItemDetails () {
       <Breadcrumb />
       <section className='container'>
         <div className='card'>
-          {item !== null
-            ? (
-              <>
-                <section className='details-section d-flex'>
-                  <img src={item?.picture} alt={item?.title} />
-                  <div className='details-price mt-1 mr-1'>
-                    <span>
-                      {item?.condition === 'new' ? 'Nuevo' : 'Usado'} - {item?.sold_quantity} vendidos
-                    </span>
-                    <h3 className='mt-1 mb-1'>{item?.title}</h3>
-                    <h1 className='mt-1 mb-2 d-flex'>{priceFormatter(item?.price.amount?.toFixed(0))}
-                      <span className='details-decimals'>
-                        {`${item?.price.decimals > 10 ? item?.price.decimals : `${item?.price.decimals}0`}`}
-                      </span>
-                    </h1>
-                    <div className='btn-primary'>Comprar</div>
-                  </div>
-                </section>
-                <section className='details-description ml-1'>
-                  <h2 className='mb-2'>Descripción del producto</h2>
-                  <p className='mb-2'>{item?.description}</p>
-                </section>
-              </>
-              )
+          {(item !== null)
+            ? (item?.error)
+                ? (<h2 style={{ textAlign: 'center' }}>Parece que este producto no existe</h2>)
+                : (
+                  <>
+                    <section className='details-section d-flex'>
+                      <img src={item?.picture} alt={item?.title} />
+                      <div className='details-price mt-1 mr-1'>
+                        <span>
+                          {item?.condition === 'new' ? 'Nuevo' : 'Usado'} - {item?.sold_quantity} vendidos
+                        </span>
+                        <h3 className='mt-1 mb-1'>{item?.title}</h3>
+                        <h1 className='mt-1 mb-2 d-flex'>{priceFormatter(item?.price.amount?.toFixed(0))}
+                          <span className='details-decimals'>
+                            {`${item?.price.decimals > 10 ? item?.price.decimals : `${item?.price.decimals}0`}`}
+                          </span>
+                        </h1>
+                        <div className='btn-primary'>Comprar</div>
+                      </div>
+                    </section>
+                    <section className='details-description ml-1'>
+                      <h2 className='mb-2'>Descripción del producto</h2>
+                      <p className='mb-2'>{item?.description}</p>
+                    </section>
+                  </>
+                  )
             : <></>}
         </div>
       </section>
